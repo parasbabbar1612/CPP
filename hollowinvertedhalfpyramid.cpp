89e56738692554e3b7220907d6f768225f18bc67
parasbabbar1612 <54595137+parasbabbar1612@users.noreply.github.com>
2019-10-26 18:53:21 -0400

    Create hollowinvertedhalfpyramid.cpp
    Inverted Hollow Half Pyramid
    *****
    *   *
    * *
    *

---
+++ hollowinvertedhalfpyramid.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int length;
+cout<<"\n Enter the size of pyramid";
+cin>>length;
+int i,j;
+for (i=0;i<length;i++)
+{
+    for (j=0;j<length-i;j++)
+    {
+        	if (i==0)
+        	{
+        	    cout<<"*";
+        	}
+        	else if (i>0 && (j == 0 || j == length-i-1))
+			{
+				cout<<"*";
+        	}
+			else
+			{
+			cout<<" ";
+			}
+		}
+	cout<<"\n";
+}
+}
