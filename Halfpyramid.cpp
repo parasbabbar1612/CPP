dc8d9f4158540ba0fdadca44a51670c551fc7e1d
parasbabbar1612 <54595137+parasbabbar1612@users.noreply.github.com>
2019-10-26 18:29:19 -0400

    Create Halfpyramid.cpp
    Half pyramid using stars.
    *
    **
    ***
    ****

---
+++ Halfpyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int length;
+cout<<"\n Enter the size of pyramid";
+cin>>length;
+int i,j;
+for (i=1;i<=length;i++)
+{
+    for (j=1;j<=i;j++)
+    {
+        	cout<<"*";
+        	
+		}
+	cout<<"\n";
+}
+}
