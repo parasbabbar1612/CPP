56de0d144c4251a9e3675d36a5778655f5466aec
parasbabbar1612 <54595137+parasbabbar1612@users.noreply.github.com>
2019-10-26 16:37:39 -0400

    Create commaoperator.cpp
    Implementation of comma "," operator

---
+++ commaoperator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int n;
+int a,b;
+cout<<"\n Enter a number 1";
+cin>>a;
+cout<<"\n Enter a number 2";
+cin>>b;
+int c=0;
+int sum=0;
+sum=(a=a+10,b=b+10,c=a+b);
+cout<<sum;
+}
