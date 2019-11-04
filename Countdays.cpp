e36fd0d665c8d58e4130281976eeda6d575f40cf
parasbabbar1612 <54595137+parasbabbar1612@users.noreply.github.com>
2019-11-04 04:28:19 -0500

    Create Countdays.cpp
    Program to count number of days passed by in a year from date.

---
+++ Countdays.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int dd,mm,yy,total;
+    int A[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    cout<<"\n Enter dd";
+    cin>>dd;
+    cout<<"\n Enter mm";
+    cin>>mm;
+    cout<<"\n Enter yy";
+    cin>>yy;
+    total=dd;
+    for(int i=0;i<mm-1;i++) total+=A[i];
+    if (yy%4==0) total+=1;
+    cout<<"Total no of days are"<<" "<<total;
+}
