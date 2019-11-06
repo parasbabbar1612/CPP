c55582ce108ddd4b69a74e2687f7cfb5122510fd
parasbabbar1612 <54595137+parasbabbar1612@users.noreply.github.com>
2019-11-05 19:27:09 -0500

    Create MinimumStops.cpp
    Suppose you and your friends need to drive across the country. You want to drive as much as you can during the day, but decide that you will rest every night. You have identified a list of suitable sleeping points along the drive and need a system
    for determining when to stop each evening. You decide to use a greedy approach.Design a greedy algorithm that minimizes the number of stops necessary for the trip

---
+++ MinimumStops.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int n,d;;
+cout<<"\n Enter number of possible stopping point";
+cin>>n;
+int A[n];
+cout<<"\n Enter the stopping points";
+for(int j=0;j<n;j++)
+{
+cin>>A[j]; 
+}
+cout<<"\n Enter the maximum distance that can be travelled in a day";
+cin>>d;
+for (j=1;j<n;j++)
+{
+A[j]=A[j]-A[j-1];
+}
+int sum=0,count=0;
+for (i=0;i<=n;i++)
+    {
+        
+        if (A[i]==d) count+=1;
+        else
+        {
+        count+=1;    
+        sum=0;
+        while (sum<=d)
+        {
+        sum+=A[i];
+        ++i;
+        }
+        --i;
+        }
+    }    
+cout<<"\n Mimimum Number of stops = "<<count;
+    }
