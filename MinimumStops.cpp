d7ba1a59c29f3477ba177e16c75011e3a973f30c
parasbabbar1612 <54595137+parasbabbar1612@users.noreply.github.com>
2019-11-12 13:21:09 -0500

    Update MinimumStops.cpp

--- MinimumStops.cpp
+++ MinimumStops.cpp
@@ -4,23 +4,26 @@ using namespace std;
 
 int main()
 {
-int n,d;;
+int n,d,i,j,distance;
 cout<<"\n Enter number of possible stopping point";
 cin>>n;
 int A[n];
+cout<<"\n Enter the maximum distance that can be travelled in a day";
+cin>>d;
+cout<<"\n Enter total journey distance";
+cin>>distance;
 cout<<"\n Enter the stopping points";
 for(int j=0;j<n;j++)
 {
 cin>>A[j]; 
 }
-cout<<"\n Enter the maximum distance that can be travelled in a day";
-cin>>d;
+A[n+1]=distance;
 for (j=1;j<n;j++)
 {
 A[j]=A[j]-A[j-1];
 }
 int sum=0,count=0;
-for (i=0;i<=n;i++)
+for (i=0;i<n;i++)
     {
         
         if (A[i]==d) count+=1;
