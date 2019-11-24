2e0040a8c3b9389a8287c4d59d8cad39257def97
parasbabbar1612 <54595137+parasbabbar1612@users.noreply.github.com>
2019-11-24 17:39:27 -0500

    Update linkedlist1.cpp
    Creating 'n' user defined nodes in a linked list and deleting  a node by searching the value given by user

--- linkedlist1.cpp
+++ linkedlist1.cpp
@@ -6,14 +6,14 @@ class node
 {
 public:
 	int data;
-	node *next;
+	node* next;
 };
 node* head = NULL;
 node* tail = NULL;
 
 void addnode(int val)
 {
-	node *temp = new node;
+	node* temp = new node;
 	temp->data = val;
 	temp->next = NULL;
 	if (head == NULL || tail == NULL)
@@ -22,17 +22,37 @@ void addnode(int val)
 		tail = temp;
 		temp = NULL;
 	}
-   else
+	else
 	{
 		tail->next = temp;
 		tail = temp;
 	}
-	
-}
 
+}
+void delnode(int v)
+{
+	node* s = head;
+	int count = 0;
+	while (s != NULL)
+	{
+		if (s->data == v)
+		{
+			count++;
+			s->data = s->next->data;
+			s->next = s->next->next;
+		}
+
+		else s = s->next;
+	}
+	if (count > 0) cout << "\n Found and deleted";
+	else cout << "\n Not found";
+	cout << "\n The list after insertion and deletion is:" << endl;
+
+
+}
 int main()
 {
-	int n,val;
+	int n, val, del;
 	cout << "\n Enter the number of nodes u want to enter";
 	cin >> n;
 	for (int i = 0; i < n; i++)
@@ -40,11 +60,14 @@ int main()
 		cout << "\n Enter node";
 		cin >> val;
 		addnode(val);
-		}
-	    node* p = head;
-		while (p != NULL)
-		{
-			cout << p->data << endl;
-			p = p->next;
-		}	
+	}
+	node* p = head;
+	cout << "\n Enter value to be deleted";
+	cin >> del;
+	delnode(del);
+	while (p != NULL)
+	{
+		cout << p->data << endl;
+		p = p->next;
+	}
 }
