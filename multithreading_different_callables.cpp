04c915238e303ee297f63789dbb6f066c175785e
parasbabbar1612 <54595137+parasbabbar1612@users.noreply.github.com>
2021-02-11 23:06:32 -0500

    Update multithreading_different_callables.cpp

--- multithreading_different_callables.cpp
+++ multithreading_different_callables.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 #include<thread>
 #include<vector>
+#include<chrono>
 
 using namespace std;
+
+class test_c {
+public:
+	test_c() {};
+	void check_larger(const int& a, const int& b) {
+		int larger = a > b ? a : b;
+		cout << larger << " Using member function ";
+	}
+};
 
 class Comp {
 public:
@@ -20,10 +30,15 @@ void check_large(const int& a, const int& b) {
 int main() {
 	Comp c1;
 	int a = 10, b = 11;
+	test_c obj;
 	check_large(a, b);
-	thread t1{ c1,a,b };
-	thread t2{ [](const int& a,const int& b) {int larger = a > b ? a : b;cout << larger << " Using lambda "; },a,b };
+	thread t1{ c1,10,11 };
+	thread t2{ [](const int& a,const int& b) {int larger = a > b ? a : b;cout << larger << " Using lambda "; },10,11 };
+	thread t3(&test_c::check_larger, ref(obj), a, b);
+	cout << "Waiting for threads to finish....." << endl;
+	this_thread::sleep_for(chrono::seconds(5));
 	t1.join();
 	t2.join();
+	t3.join();
 	return 0;
 }
